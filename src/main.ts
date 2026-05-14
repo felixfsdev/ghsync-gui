2c207ce9ff13510ee3a58b2320d5428176aaac9a
FaseehFS <faseeh1080@gmail.com>
2026-05-14 12:18:02 +0530

    remove menu from mainwindow

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,8 @@ const createWindowAndReturn = () => {
     },
   });
 
+  mainWindow.removeMenu();
+
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
