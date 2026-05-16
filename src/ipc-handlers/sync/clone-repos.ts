30a1b209e769bc47f0810164df23e81dd0459717
EffessDev <faseeh1080@gmail.com>
2026-05-16 16:19:46 +0530

    refactor: separate get repos function

--- src/ipc-handlers/sync/clone-repos.ts
+++ src/ipc-handlers/sync/clone-repos.ts
@@ -2,15 +2,9 @@ import Store from "electron-store";
 import * as path from "path";
 import { promises as fs } from "fs";
 import { runGitCommand } from "./run-git-command";
+import { Repo, getAllRepos } from "./get-all-repos";
 
 const store = new Store() as any;
-
-type Repo = {
-  name: string; // shoop
-  full_name: string; // faseehfs-archive/shoop
-  url: string; // https://github.com/faseehfs-archive/shoop.git
-  path: string; // C:\\Users\\fasee\\Temp\\backups\\faseehfs-archive\\shoop
-};
 
 export default async function cloneRepos(
   mainWindow: Electron.BrowserWindow,
@@ -75,57 +69,6 @@ export default async function cloneRepos(
     failedToClone: failedToClone,
     ignored: ignored,
   };
-}
-
-async function getAllRepos(
-  pat: string,
-  storagePath: string,
-): Promise<{
-  success: boolean;
-  message: string;
-  repos: Repo[] | null;
-}> {
-  try {
-    const response = await fetch(
-      "https://api.github.com/user/repos?per_page=100",
-      {
-        method: "GET",
-        headers: {
-          Authorization: `token ${pat}`,
-          "X-GitHub-Api-Version": "2022-11-28",
-        },
-      },
-    );
-
-    if (!response.ok) {
-      return {
-        success: false,
-        message: `Failed to fetch repos: ${response.status} ${response.statusText}`,
-        repos: null,
-      };
-    }
-
-    const reposJSON = await response.json();
-    const repos: Repo[] = reposJSON.map((repo: any) => ({
-      name: repo.name,
-      full_name: repo.full_name,
-      url: repo.clone_url,
-      path: path.join(storagePath, ...repo.full_name.split("/")),
-    }));
-
-    return {
-      success: true,
-      message: "Repos fetched successfully",
-      repos,
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      success: false,
-      message: "Failed to fetch repos due to an unknown error",
-      repos: null,
-    };
-  }
 }
 
 async function cloneReposFromRepos(
